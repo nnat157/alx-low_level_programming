7b0bf1a22fca5cfbbd839052e4897ced41685e32
Najib <najibtackie55@gmail.com>
2022-10-15 07:04:31 -0700

    Task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,11 +1,11 @@
 #include <stdio.h>
 /**
  * main - Contains all relevant blocks of code
+ *
  * Return: Must always return a value of 0
  */
-
 int main(void)
 {
-	puts("\"Programming is like building a multilingual puzzle");
-	return (0);
+puts("\ "Programming is like building a multilingual puzzle");
+return(0);
 }
