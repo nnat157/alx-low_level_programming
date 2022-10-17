e591e61b233351fbf067eac72d402431c1ef804b
Najib <najibtackie55@gmail.com>
2022-10-17 07:46:38 -0700

    Task 6

--- 0x01-variables_if_else_while/6-print_numberz.c
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -5,14 +5,16 @@
  * Return: Always 0
  */
 int main(void)
-
 {
-
 	int num;
 
-	for (num = 0; num < 10; num++)
-
-		putchar((num % 10) * '0');
+	num = 0;
+	while
+		(num < 10)
+		{
+			putchar(num + '0');
+			num++;
+		}
 
 	putchar('\n');
 	return (0);
